f387ce88cb0247538144eba56a858dda9744de0d
JuanGarza97 <juancgarsa@gmail.com>
2021-03-23 16:05:03 -0600

    MachineRegister styles

--- Scripts/registerMachines.js
+++ Scripts/registerMachines.js
@@ -14,3 +14,15 @@ function process(event) {
     info.style.display = "";
     info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`;
 }  
+
+function tabE(obj,e){ 
+    var e=(typeof event!='undefined')?window.event:e;// IE : Moz 
+    if(e.keyCode==13){ 
+      var ele = document.forms[0].elements; 
+      for(var i=0;i<ele.length;i++){ 
+        var q=(i==ele.length-1)?0:i+1;// if last element : if any other 
+        if(obj==ele[i]){ele[q].focus();break} 
+      } 
+   return false; 
+    } 
+   } 
