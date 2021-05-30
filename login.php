a07bb93bf946fe4bc040593da2ebe7a02eddacfd
RicarlOz <A01235366@itesm.mx>
2021-05-29 20:56:38 -0500

    Users can already loging

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+include('ConnectionDB.php');
+
+$found = FALSE;
+$email = $_POST['email'];
+$pwrd = $_POST['password'];
+
+$sql = "SELECT * FROM users";
+
+if (!($result = consultDB($sql))) {
+    echo "Problemas con la conexión con la base de datos.";
+}
+else {
+    while ($row = mysqli_fetch_array($result)) {
+        if ($email == $row["Email"] && $pwrd == $row["Contraseña"]) {
+            $found = TRUE;
+        }
+    }
+}
+echo $found;
+
+?>
